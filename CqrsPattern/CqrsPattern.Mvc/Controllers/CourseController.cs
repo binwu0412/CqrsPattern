d2cad17cfe335a5971fd46c914b42466f52325ee
binwu0412 <ding_60@hotmail.com>
2021-02-20 17:57:30 -0500

    folder structure for cqrs

---
+++ CqrsPattern/CqrsPattern.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CqrsPattern.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CqrsPattern.Mvc.Controllers
+{
+    public class CourseController : Controller
+    {
+        public ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public IActionResult Index()
+        {
+            var courseModelView = _courseService.GetCourses();
+
+            return View(courseModelView);
+        }
+    }
+}
