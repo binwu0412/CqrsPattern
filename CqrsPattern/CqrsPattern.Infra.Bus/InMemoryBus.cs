b004e829d4d8af12432a70c037ce0912895fd907
binwu0412 <ding_60@hotmail.com>
2021-02-20 19:16:08 -0500

    add domain.core and implement InMemoryBus

--- CqrsPattern/CqrsPattern.Infra.Bus/InMemoryBus.cs
+++ CqrsPattern/CqrsPattern.Infra.Bus/InMemoryBus.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using CqrsPattern.Domain.Core.Bus;
+using CqrsPattern.Domain.Core.Commands;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CqrsPattern.Infra.Bus
 {
-    public class InMemoryBus
+    public class InMemoryBus : IMediatorHandler
     {
+        private readonly IMediator _mediator;
+
+        public InMemoryBus(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public Task SendCommand<T>(T command) where T : Command
+        {
+            return _mediator.Send(command);
+        }
     }
 }
